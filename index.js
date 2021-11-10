580be47c7ded51ec431bdb18315e90a3c8d3f714
Artur <nikitsin.artur@gmail.com>
2021-11-10 10:30:47 +0300

    feat: add checking of read & write files

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ const { createShiftsChain, parseOption, throwError } = require('./src/utils')
 const customStreams = require('./src/customSteams')
 
 const appArguments = process.argv
-
 const configOption = parseOption({ options: appArguments, criteria: '-c' })
 if (!configOption) {
     throwError('Config option "-c" not provided')
 }
+
 const pathRead = parseOption({ options: appArguments, criteria: '-i' })
 const pathWrite = parseOption({ options: appArguments, criteria: '-o' })
+
+if (pathRead) {
+    if (!fs.existsSync(pathRead)) {
+        throwError('read not found')
+    }
+}
+if (pathWrite) {
+    if (!fs.existsSync(pathWrite)) {
+        throwError('write not found')
+    }
+}
+
 const options = {}
-
-console.log('@@@', configOption)
-
-const write = fs.createWriteStream(pathWrite, options)
-const read = fs.createReadStream(pathRead, options)
+const write = pathWrite
+    ? fs.createWriteStream(pathWrite, options)
+    : fs.createWriteStream('output.txt', options)
+const read = pathRead ? fs.createReadStream(pathRead, options) : process.stdin
 
 const shiftsChain = createShiftsChain(configOption)
 
