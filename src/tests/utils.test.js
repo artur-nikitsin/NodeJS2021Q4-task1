39b1a4542c86a22611e595738a7d961914e25a7a
Artur <nikitsin.artur@gmail.com>
2021-11-17 17:05:13 +0300

    feat: add createShiftsChain & parseOption utils function test

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -32,3 +32,63 @@ describe('test throwError util function', () => {
         mockExit.mockRestore()
     })
 })
+
+describe('test createShiftsChain util function', () => {
+    it('returned shift must be 2 ', () => {
+        const expectedResult = [
+            1,
+            -1,
+            'reverse',
+            8,
+            -8,
+            'reverse',
+            -8,
+            -8,
+            1,
+            'reverse',
+        ]
+        expect(createShiftsChain('C1-C0-A-R1-R0-A-R0-R0-C1-A')).toMatchObject(
+            expectedResult
+        )
+    })
+
+    it('throw error on invalid config', () => {
+        const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {
+            throw new Error('Error message')
+        })
+        expect(() => {
+            createShiftsChain('C2-C0')
+        }).toThrow('Error message')
+        expect(mockExit).toHaveBeenCalledWith(1)
+        mockExit.mockRestore()
+    })
+})
+
+describe('test parseOption util function', () => {
+    it('throw error on options duplicated', async () => {
+        const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {
+            throw new Error('Error message')
+        })
+        const options = [
+            '-c',
+            'C1-C0-A',
+            '-i',
+            'input.txt',
+            '-o',
+            'output.txt',
+            '-c',
+            'C1-C0-A',
+        ]
+        expect(() => {
+            parseOption({ options, criteria: '-c' })
+        }).toThrow('Error message')
+        expect(mockExit).toHaveBeenCalledWith(1)
+        mockExit.mockRestore()
+    })
+
+    it('returned shift must be 2 ', () => {
+        const expectedString = 'C1-C0-A'
+        const options = ['-c', expectedString]
+        expect(parseOption({ options, criteria: '-c' })).toEqual(expectedString)
+    })
+})
