e09d63ae36eecef1dd10983ff5decd86e7b54a81
Artur <nikitsin.artur@gmail.com>
2021-11-17 14:48:44 +0300

    feat: add test for shiftPosition util function

---
+++ src/tests/utils.test.js
@@ -0,0 +1,16 @@
+const { shiftPosition } = require('../utils')
+const alphabet = require('../alphabet')
+
+describe('test shiftPosition util function', () => {
+    it('returned shift must be 2 ', () => {
+        expect(shiftPosition({ prevIndex: 1, shift: 1, alphabet })).toEqual(2)
+    })
+    it('returned shift must be 0 ', () => {
+        expect(shiftPosition({ prevIndex: 25, shift: 1, alphabet })).toEqual(0)
+    })
+    it('returned shift must be 2 ', () => {
+        expect(
+            shiftPosition({ prevIndex: 0, shift: 'reverse', alphabet })
+        ).toEqual(25)
+    })
+})
