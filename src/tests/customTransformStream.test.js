2ffc778f2e31664ae624bb76abea1c47608b04c7
Artur <nikitsin.artur@gmail.com>
2021-11-20 11:05:03 +0300

    feature: add e2e tests for general error & success scenarios

--- src/tests/customTransformStream.test.js
+++ src/tests/customTransformStream.test.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const customStreams = require('../customSteams')
 const { createMockExit } = require('../testUtils/testUtils')
 const { throwError } = require('../utils')
@@ -6,11 +7,14 @@ const shift = 8
 const transformStream = customStreams.createCustomTransformStream({
     shift,
 })
+const read = fs.createReadStream('./src/tests/fixtures/testInput.txt', {})
+const write = fs.createWriteStream('./src/tests/fixtures/testOutput.txt', {})
 
 describe('test transformStream', () => {
     it('tests transformStream', async () => {
-        transformStream.on('data', (something) => {
+        transformStream.on('transform', (something) => {
             console.log(something)
         })
+        read.pipe(transformStream).pipe(write)
     })
 })
